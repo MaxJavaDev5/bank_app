34cb5b622b725f0f815f852a34a3f095d6a91c63
Maksim <max2008sh@gmail.com>
2026-06-22 21:47:27 +0200

    add Eureka discovery server

---
+++ discovery-service/src/test/java/ru/practicum/discovery/DiscoveryServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.practicum.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+        
+    }
+}
