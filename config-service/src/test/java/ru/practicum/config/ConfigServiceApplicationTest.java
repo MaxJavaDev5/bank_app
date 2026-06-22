dfae932ce92b1be701573215f6e943a47b0adc70
Maksim <max2008sh@gmail.com>
2026-06-22 21:51:25 +0200

    add config server

---
+++ config-service/src/test/java/ru/practicum/config/ConfigServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.practicum.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
