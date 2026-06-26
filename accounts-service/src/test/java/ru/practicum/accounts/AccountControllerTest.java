21ff061230a5da75ed00f897c93a060f840dc22d
Maksim <max2008sh@gmail.com>
2026-06-26 19:00:04 +0200

    add prod health profile and replace deprecated MockBean

--- accounts-service/src/test/java/ru/practicum/accounts/AccountControllerTest.java
+++ accounts-service/src/test/java/ru/practicum/accounts/AccountControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
@@ -47,7 +47,7 @@ class AccountControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private AccountService accountService;
 
     @Test
